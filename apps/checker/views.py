b5c6bb7fc77d0637b37190f1920b506ce2887b3b
xwp <xwp_fullstack@163.com>
2018-04-26 14:15:11 +0800

    业绩量化部分

--- apps/checker/views.py
+++ apps/checker/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from django.views.generic import View
 # Create your views here.
+
+
+class Scinece(View):
+    def get(self,request):
+        pass
+    def post(self,request):
+        pass
