f178a91a6e8b9e4874cc8094d4513dec4bbfc11a
xwp <xwp_fullstack@163.com>
2018-05-03 16:50:02 +0800

    业绩录入2

--- apps/teacher/models.py
+++ apps/teacher/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from apps.user.models import User
 # Create your models here.
 # 教学工作量
 class Teacher_work(models.Model):
@@ -43,4 +43,56 @@ class Teacher_book(models.Model):
             (3, '中文编写'),
             (4, '外文编写')
             )
-    book_level=models.SmallIntegerField(default=1,choices=Choice,verbose_name='1.教材级别系数、语言类别系数表')
+    # book_level=models.SmallIntegerField(default=1,choices=Choice,verbose_name='1.教材级别系数、语言类别系数表')
+    book_level=models.ForeignKey('Book_level',verbose_name='.教材级别系数、语言类别系数表')
+    book_auth=models.ForeignKey('Book_auth',verbose_name='主编部分')
+    book_lixiang=models.ForeignKey('Book_lixiang',verbose_name='立项系数')
+    class Meta:
+        db_table = 'teacher_book'
+        verbose_name = '教材业绩量化表'
+        verbose_name_plural = verbose_name
+
+
+class Book_level(models.Model):
+    book_level_name=models.CharField(max_length=50, verbose_name='等级名称')
+    book_jidian=models.IntegerField(default=0, verbose_name='业绩点')
+    class Meta:
+        db_table = 'book_level'
+        verbose_name = '教材级别系数、语言类别系数表'
+        verbose_name_plural = verbose_name
+class Book_auth(models.Model):
+    book_auth_name = models.CharField(max_length=50, verbose_name='主编名称')
+    auth_jidian = models.IntegerField(default=0, verbose_name='业绩点')
+
+    class Meta:
+        db_table = 'book_auth'
+        verbose_name = '主编部分'
+        verbose_name_plural = verbose_name
+class Book_lixiang(models.Model):
+    book_lixiang_name = models.CharField(max_length=50, verbose_name='立项名')
+    book_lixiang_math = models.CharField(max_length=50, verbose_name='立项系数')
+
+    class Meta:
+        db_table = 'book_lixiang'
+        verbose_name = '立项系数'
+        verbose_name_plural = verbose_name
+
+
+
+# 业绩表
+class Teacher_count(models.Model):
+    usernum=models.CharField(max_length=50,verbose_name='用户学号')
+    count_jidian=models.IntegerField(default=0,verbose_name='个人业绩点')
+    rate_jidian=models.ForeignKey('Rate_jidian',verbose_name='额定业绩点')
+    class Meta:
+        db_table = 'teacher_count'
+        verbose_name = '教师业绩点统计'
+        verbose_name_plural = verbose_name
+
+class Rate_jidian(models.Model):
+    name=models.CharField(max_length=50, verbose_name='职称名')
+    jiidan=models.IntegerField(default=0,verbose_name='职称对应业绩点')
+    class Meta:
+        db_table = 'rate_jidian'
+        verbose_name = '职称对应业绩点'
+        verbose_name_plural = verbose_name
