2bb584b35879229444765b91908470a86991e7aa
xwp <xwp_fullstack@163.com>
2018-05-04 09:30:38 +0800

    业绩录入3

--- TeacherManager/urls.py
+++ TeacherManager/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^user/',include('apps.user.urls',namespace='user')),
     url(r'^scence/',include('apps.register.urls',namespace='scence'))
+
 ]
