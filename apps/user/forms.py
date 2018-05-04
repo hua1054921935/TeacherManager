1d88a5987757b5c051601a6a767ddbd043d6e448
xwp <xwp_fullstack@163.com>
2018-05-04 09:28:50 +0800

    业绩点3

--- apps/user/forms.py
+++ apps/user/forms.py
@@ -1,25 +1,26 @@
-# # coding=utf-8
-# from django.contrib.auth.forms import UserCreationForm
-# from .models import User
-# from django.contrib.auth.models import BaseUserManager
-#
-# class MyUserManager(BaseUserManager):   # 定义创建登录用户和超级用户时需要的字段
-#     def create_user(self, email, user_num, professor,role,password=None):
-#         """
-#         Creates and saves a User with the given email, date of
-#         birth and password.
-#         """
-#         if not user_num:
-#             raise ValueError('Users must have an email address')
-#
-#         user = self.model(
-#             email=self.normalize_email(email),
-#             user_num=user_num,
-#             role=role,
-#             professor=professor
-#         )
-#
-#         user.set_password(password)
-#         user.save(using=self._db)
-#         return user
-#
+# coding=utf-8
+from django.contrib.auth.forms import UserCreationForm
+from .models import User
+from django.contrib.auth.models import BaseUserManager
+
+class MyUserManager(BaseUserManager):   # 定义创建登录用户和超级用户时需要的字段
+    def create_user(self, email, user_num, professor,role,password=None):
+        print(66666666)
+        """
+        Creates and saves a User with the given email, date of
+        birth and password.
+        """
+        if not user_num:
+            raise ValueError('Users must have an email address')
+
+        user = self.model(
+            email=self.normalize_email(email),
+            user_num=user_num,
+            role=role,
+            professor=professor
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
