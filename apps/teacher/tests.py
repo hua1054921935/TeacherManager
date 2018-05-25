a1f9e40d5db849ac44790659fb10c43d1debcd0e
xwp <xwp_fullstack@163.com>
2018-05-25 15:04:07 +0800

    业绩点7

--- apps/teacher/tests.py
+++ apps/teacher/tests.py
@@ -2,5 +2,8 @@ from django.test import TestCase
 # from django.contrib.auth.models import User
 from apps.user.models import Role,User
 # Create your tests here.
-role=Role.objects.get(id=1)
-user=User.objects.create_superuser(username='admin',password=123456,role=role)
+role=Role.objects.get(id=3)
+print(role.role_name)
+user=User.objects.create_superuser('admin',123456,role)
+
+print('wanchneg')
